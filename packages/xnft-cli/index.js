872f4279795b5a1b1cab80fa8438a1fb9c5104b2
Ajeet Sinha <honeydreamssoftwares@gmail.com>
2023-09-27 17:06:01 +0530

    initial

---
+++ packages/xnft-cli/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node --max-old-space-size=10240
+
+// using JS for now because there are race-condition issues
+// with compiling typescript before running in the monorepo
+const { program } = require("commander");
+const fs = require("fs");
+const bundle = require("./new");
+
+const pkg = JSON.parse(fs.readFileSync(__dirname + "/package.json").toString());
+program.version(pkg.version);
+bundle(program);
+program.parse();
+
